7bc93d8c33a1bf38c473240dca0e8a86f9fa5305
Trajuan <tr4juansmith@gmail.com>
2026-03-18 23:10:09 -0400

    Add insert, query, and Habit completion  methods

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -1,12 +1,20 @@
+// Trajuan Smith
+// Helper class to manage our SQLite db setup and connection.
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:habit_mastery_league/models/habits.dart';
 
 class DatabaseHelper {
+
+  // Ensure only a single shared helper object 'instance', exists
+  // and only ONE database connection, '_database', is maintained.
+  DatabaseHelper._init();
   static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
 
-  DatabaseHelper._init();
-
+  // If db exists return it,
+  // if not, open/create it, then return it.
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDB('habit_tracker.db');
@@ -24,6 +32,7 @@ class DatabaseHelper {
     );
   }
 
+  // Create both the habits and habit_logs tables 
   Future<void> _createDB(Database db, int version) async {
     await db.execute('''
       CREATE TABLE habits (
@@ -42,6 +51,11 @@ class DatabaseHelper {
       )
     ''');
 
+    // Wrap habit_id and log_date with UNIQUE to prevent duplicate 
+    // same-day logs.
+    //
+    // Also using FOREIGN KEY connect relevant habit to its log,
+    // Hence on habit deletion its logs are also deleted.
     await db.execute('''
       CREATE TABLE habit_logs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -55,4 +69,60 @@ class DatabaseHelper {
       )
     ''');
   }
+
+  Future<int> insertHabit(Habit habit) async {
+    final db = await instance.database;
+    return await db.insert('habits', habit.toMap());
+  }
+
+  // Get all rows in the habits table, then 
+  // loop through each row and convert them to 
+  // Habit Objects, then convert all Habit Objects
+  // to a List.
+  Future<List<Habit>> getAllHabits() async {
+    final db = await instance.database;
+    final result = await db.query('habits');
+    return result.map((map) => Habit.fromMap(map)).toList();
+  }
+
+  // Checks id using Where clause, then
+  // if found, 'result.first' should return 
+  // the first and only element in the list 
+  // stored in result. If not found return Null.
+  Future<Habit?> getHabitById(int id) async {
+    final db = await instance.database;
+
+    final result = await db.query(
+      'habits',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (result.isNotEmpty) {
+      return Habit.fromMap(result.first);
+    } else {
+      return null;
+    }
+  }
+
+  // Mark a habit as complete in the habit_logs
+  // create and insert a log map into the table
+  Future<int> markHabitCompleteForDate(int habitId, String logDate) async {
+    final db = await instance.database;
+
+    final log = {
+      'habit_id': habitId,
+      'log_date': logDate,
+      'completed': 1,
+      'used_streak_shield': 0,
+      'completed_at': DateTime.now().toIso8601String(),
+    };
+
+    return await db.insert(
+      'habit_logs',
+      log,
+      conflictAlgorithm: ConflictAlgorithm.ignore,
+    );
+  }
+
 }
