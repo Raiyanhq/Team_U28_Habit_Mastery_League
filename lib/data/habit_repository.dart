80f2251ba6118bee955bec9735cceef4b149f908
Trajuan <tr4juansmith@gmail.com>
2026-03-19 10:23:33 -0400

    Create initial HabitRepository to separate app logic from raw database operations

---
+++ lib/data/habit_repository.dart
@@ -0,0 +1,37 @@
+import '../models/habits.dart';
+import '../models/habit_log.dart';
+import 'database_helper.dart';
+
+// This will be the layer in between the UI layer and raw databse
+class HabitRepository {
+  final DatabaseHelper _dbHelper = DatabaseHelper.instance;
+
+  Future<int> createHabit(Habit habit) async {
+    return await _dbHelper.insertHabit(habit);
+  }
+
+  Future<List<Habit>> fetchAllHabits() async {
+    return await _dbHelper.getAllHabits();
+  }
+
+  Future<Habit?> fetchHabitById(int id) async {
+    return await _dbHelper.getHabitById(id);
+  }
+
+  Future<int> editHabit(Habit habit) async {
+    return await _dbHelper.updateHabit(habit);
+  }
+
+  Future<int> removeHabit(int id) async {
+    return await _dbHelper.deleteHabit(id);
+  }
+
+  Future<int> completeHabitForToday(int habitId) async {
+    final today = DateTime.now().toIso8601String().split('T')[0];
+    return await _dbHelper.markHabitCompleteForDate(habitId, today);
+  }
+
+  Future<List<HabitLog>> fetchLogsForHabit(int habitId) async {
+    return await _dbHelper.getLogsForHabit(habitId);
+  }
+}
